5d34026e668712872b8aa046817e66e3fba6a76d
Julian Hammer <jh@moinjulian.com>
2024-04-02 19:09:54 +0100

    Create insert-course.mongodb.js

---
+++ insert-course.mongodb.js
@@ -0,0 +1,10 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('course');
+
+// Create a new document in the collection.
+db.getCollection('courses').insertOne({
+
+});
