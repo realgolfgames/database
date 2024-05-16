932f154373eb61a1091a4a9eacd4a788996f272d
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2024-05-16 13:00:48 +0100

    Add styles

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '$lib/scss/app.scss';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		max-width: 40rem;
+		margin: 0 auto;
+        padding-inline: 1rem;
+
+        @media screen and (max-width: 767px) {
+			margin-bottom: 20px;
+		}
+	}
+</style>
